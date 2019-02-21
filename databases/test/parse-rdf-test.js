9ba30903f389590901c3332950de312d99d61eb5
Carlos García González <alu0100898026@ull.edu.es>
2019-02-21 21:10:23 +0000

    Pasa primer test, segundo test añadido

--- databases/test/parse-rdf-test.js
+++ databases/test/parse-rdf-test.js
@@ -2,11 +2,17 @@
 
 const fs = require('fs');
 const expect = require('chai').expect;
+const parseRDF = require('../lib/parse-rdf.js');
 
 const rdf = fs.readFileSync(`${__dirname}/pg132.rdf`);
 
 describe('parseRDF', () => {
 	it('should be a function', () => {
 		expect(parseRDF).to.be.a('function');
+	});
+
+	it('shoud parse RDF content', () => {
+		const book = parseRDF(rdf);
+		expect(book).to.be.an('object');
 	});
 });
